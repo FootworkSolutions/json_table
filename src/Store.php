faf0333696abb1e306cf31229e51cec800d5a9bf
George Wilde <george.wilde@footworksolutions.co.uk>
2015-11-06 15:21:28 +0000

    Started improving coding standards for Store.
    I'm commiting and pulling this into master now to check that
    Code Climate picks up the new config.

--- src/Store.php
+++ src/Store.php
@@ -8,46 +8,43 @@ namespace JsonTable;
  */
 class Store extends Base
 {
-	/**
-	 * Load and instantiate the specified store.
-	 *
-	 * @access private
-	 *
-	 * @param string $ps_store_type The type of store to load.
-	 *
-	 * @return object The store object. Throws an exception on error.
-	 */
-	public static function load($ps_store_type)
-	{
-		// Load the abstract store file.
-		$ls_abstract_store_file = dirname(__FILE__) . "/Store/AbstractStore.php";
+    /**
+     * Load and instantiate the specified store.
+     *
+     * @access private
+     *
+     * @param string $ps_store_type The type of store to load.
+     *
+     * @return object The store object. Throws an exception on error.
+     */
+    public static function load($ps_store_type)
+    {
+        // Load the abstract store file.
+        $ls_abstract_store_file = dirname(__FILE__) . "/Store/AbstractStore.php";
 
-		if (file_exists($ls_abstract_store_file) && is_readable($ls_abstract_store_file)) {
-			include_once $ls_abstract_store_file;
-		}
-		else {
-			throw new \Exception("Could not load the abstract store file.");
-		}
+        if (!file_exists($ls_abstract_store_file) || !is_readable($ls_abstract_store_file)) {
+            throw new \Exception("Could not load the abstract store file.");
+        }
 
-		// Load the store file for the specified type.
-		$ps_store_type = ucwords($ps_store_type);
-		$ls_store_file = dirname(__FILE__) . "/Store/$ps_store_type" . "Store.php";
-		// $ls_store_file = ucwords($ls_store_file) . 'Store';
+        include_once $ls_abstract_store_file;
 
-		if (file_exists($ls_store_file) && is_readable($ls_store_file)) {
-			include_once $ls_store_file;
-		}
-		else {
-			throw new \Exception("Could not load the store file for $ps_store_type.");
-		}
+        // Load the store file for the specified type.
+        $ps_store_type = ucwords($ps_store_type);
+        $ls_store_file = dirname(__FILE__) . "/Store/$ps_store_type" . "Store.php";
 
-		// Check that the class exists.
-		$ls_store_class = "\\JsonTable\\Store\\$ps_store_type" . "Store";
+        if (!file_exists($ls_store_file) || !is_readable($ls_store_file)) {
+            throw new \Exception("Could not load the store file for $ps_store_type.");
+        }
 
-		if (!class_exists($ls_store_class)) {
-			throw new \Exception("Could not find the store class $ls_store_class");
-		}
+        include_once $ls_store_file;
 
-		return new $ls_store_class();
-	}
+        // Check that the class exists.
+        $ls_store_class = "\\JsonTable\\Store\\$ps_store_type" . "Store";
+
+        if (!class_exists($ls_store_class)) {
+            throw new \Exception("Could not find the store class $ls_store_class");
+        }
+
+        return new $ls_store_class();
+    }
 }
