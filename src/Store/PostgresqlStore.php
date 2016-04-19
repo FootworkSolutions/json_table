cc4d3c807286f8d3d36aed758ad1377136bbe2fc
George Wilde <george.wilde@footworksolutions.co.uk>
2016-04-19 17:10:33 +0100

    Refactored the postgresql store methods as they were too complex

--- src/Store/PostgresqlStore.php
+++ src/Store/PostgresqlStore.php
@@ -11,6 +11,16 @@ use \JsonTable\Base;
 class PostgresqlStore extends AbstractStore
 {
     /**
+     * @var string  The name of the table to store the data into.
+     */
+    private $tableName;
+
+    /**
+     * @var string  The name of the primary key column in the table the data is being stored in.
+     */
+    private $primaryKey;
+
+    /**
      * @var array      Data type and format metadata for each column being inserted.
      *                 The Key is the CSV column position in the file and value is an array of:
      *                     "pdo_type" - The PDO data type
@@ -18,6 +28,31 @@ class PostgresqlStore extends AbstractStore
      *                     "format" - The schema format.
      */
     private $column_metadata = [];
+
+    /**
+     * @var string  The CSV list of columns to be inserted into.
+     */
+    private $columnList;
+
+    /**
+     * @var string  The parameters to be used in the insert statement.
+     */
+    private $insertParameters;
+
+    /**
+     * @var int The current CSV row being stored.
+     */
+    private $currentCsvRow;
+
+    /**
+     * @var int The position of the current CSV row row in the CSV file.
+     */
+    private $rowNumber;
+
+    /**
+     * @var int The position of the current CSV field in the current row.
+     */
+    private $fieldNumber;
 
 
     /**
@@ -42,101 +77,24 @@ class PostgresqlStore extends AbstractStore
     /**
      * Store the data.
      *
-     * @param   string  $ps_table_name  The name of the table to save the data in. With optional schema prefix.
-     * @param   string  $ps_primary_key The name of the primary key on the table. [optional] The default is "id".
+     * @param   string  $tableName  The name of the table to save the data in. With optional schema prefix.
+     * @param   string  $primaryKey The name of the primary key on the table. [optional] The default is "id".
      *                                  The primary key does not need to be listed in the CSV if it has
      *                                  a serial associated with it.
      *
-     * @return  boolean true on success false on failure.
-     *
-     * @throws  \Exception if the row couldn't be inserted into the database.
+     * @return  boolean true
      */
-    public function store($ps_table_name, $ps_primary_key = 'id')
+    public function store($tableName, $primaryKey = 'id')
     {
-        // Open the CSV file for reading.
+        $this->tableName = (string) $tableName;
+        $this->primaryKey = (string) $primaryKey;
         Base::openFile();
-
-        // Get a list of columns being inserted into from the CSV header row.
-        $ls_column_list = implode(', ', Base::$headerColumns);
-
-        // Add the csv_row field to the column list.
-        //This field stores the CSV row number to help make error messages more useful.
-        $ls_column_list .= ', csv_row';
-
-        // Set the metadata for the CSV columns.
+        $this->setColumns();
         $this->setColumnsMetadata();
-
-        // Define the parameter list for the statement.
-        $ls_insert_parameters = implode(', ', array_fill(0, count(Base::$headerColumns), '?'));
-
-        // Add an additional parameter for the csv_row field.
-        $ls_insert_parameters .= ', ?';
-
-        // Rewind the CSV file pointer to the first line of data.
+        $this->setInsertParameters();
         Base::rewindFilePointerToFirstData();
-
-        // Set the row flag.
-        $li_row = 1;
-
-        // Read each row in the file.
-        while ($la_row = Base::loopThroughFileRows()) {
-            $la_csv_row = $la_row;
-            
-                // Set up the SQL statement for the insert.
-            $ls_insert_sql = "INSERT INTO $ps_table_name (
-                                  $ls_column_list
-                              )
-                              VALUES (
-                                  $ls_insert_parameters
-                              )
-                              RETURNING
-                                $ps_primary_key AS key";
-            $lo_statement = self::$pdoConnection->prepare($ls_insert_sql);
-
-            // Loop through each column in the CSV row.
-            $li_field_number = 1;
-
-            foreach ($la_csv_row as &$lm_field_value) {
-                // Get this column's metadata for easy access and readability.
-                $la_column_metadata = $this->column_metadata[$li_field_number];
-
-                // Do any data manipulation required on this column.
-                // If the type is date and there is a format, convert this to an ISO date.
-                if ('date' === $la_column_metadata['type'] && 'default' !== $la_column_metadata['format']) {
-                    $lm_field_value = self::isoDateFromFormat($la_column_metadata['format'], $lm_field_value);
-                }
-
-                // If the type is boolean ensure that the value is boolean
-                // as the validation will pass for "1/0", "on/off" and "yes/no".
-                if ('boolean' === $la_column_metadata['type']) {
-                    $lm_field_value = self::booleanFromFilterBooleans($lm_field_value);
-                }
-
-                // Convert empty strings or special "\N" identifiers to null.
-                if ('' === $lm_field_value || '\N' === $lm_field_value) {
-                    $lm_field_value = null;
-                }
-
-                // Bind the field to the insert statement.
-                $lo_statement->bindParam($li_field_number++, $lm_field_value, $la_column_metadata['pdo_type']);
-            }
-
-            // Bind the extra parameter for the csv_row field.
-            $lo_statement->bindParam($li_field_number, $li_row, \PDO::PARAM_INT);
-
-            // Run the statement to insert the row.
-            $la_result = $lo_statement->execute();
-
-            if (false === $la_result) {
-                // The query failed.
-                throw new \Exception("Could not insert row $li_row into the database.");
-            }
-
-            // Add this insert's primary key to the list of inserted columns.
-            $this->insertedIds[] = $lo_statement->fetch(\PDO::FETCH_ASSOC);
-
-            $li_row++;
-        }
+        $this->rowNumber = 1;
+        $this->storeCsvRows();
 
         return true;
     }
@@ -149,32 +107,113 @@ class PostgresqlStore extends AbstractStore
      */
     private function setColumnsMetadata()
     {
-        // Get the data type for each of the columns being inserted into.
         foreach (Base::$headerColumns as $li_csv_field_position => $ls_csv_column_name) {
             $la_metadata = [];
-
-            // Compensate for 1 based column reference.
             $li_csv_field_position += 1;
 
-            // Get the schema key for this column name.
             $li_schema_key = $this->getSchemaKeyFromName($ls_csv_column_name);
-
-            // Get the field object for this schema key.
             $lo_schema_field = self::$schemaJson->fields[$li_schema_key];
 
-            // Get the schema type for this column.
             $la_metadata['type'] = $this->getColumnType($lo_schema_field);
-
-            // Set the PDO data type for this column.
             $la_metadata['pdo_type'] = self::$pdo_type_mappings[$la_metadata['type']];
-
-            // Set the format for this column.
             $la_metadata['format'] = $this->getColumnFormat($lo_schema_field);
 
-            // Set the metadata for this column.
             $this->column_metadata[$li_csv_field_position] = $la_metadata;
         }
 
         return true;
+    }
+
+
+    /**
+     * Set the columns that will be inserted into.
+     * The columns include the "csv_row" field to store the CSV row
+     * number to help make error messages more useful.
+     *
+     * @return  void
+     */
+    private function setColumns()
+    {
+        $this->columnList = implode(', ', Base::$headerColumns);
+        $this->columnList .= ', csv_row';
+    }
+
+
+    /**
+     * Set the insert parameters string.
+     *
+     * @return  void
+     */
+    private function setInsertParameters()
+    {
+        $this->insertParameters = implode(', ', array_fill(0, count(Base::$headerColumns), '?'));
+        $this->insertParameters .= ', ?';
+    }
+
+
+    /**
+     * Store each of the CSV rows.
+     *
+     * @return  void
+     *
+     * @throws  \Exception if the row couldn't be inserted into the database.
+     */
+    private function storeCsvRows()
+    {
+        while ($this->currentCsvRow = Base::loopThroughFileRows()) {
+            $insertSql = "INSERT INTO $this->tableName (
+                                  $this->columnList
+                              )
+                              VALUES (
+                                  $this->insertParameters
+                              )
+                              RETURNING
+                                $this->primaryKey AS key";
+
+            $statement = self::$pdoConnection->prepare($insertSql);
+            $this->fieldNumber = 1;
+
+            foreach ($this->currentCsvRow as &$fieldValue) {
+                $columnMetadata = $this->column_metadata[$this->fieldNumber];
+                $fieldValue = $this->updateFieldValue($columnMetadata, $fieldValue);
+                $statement->bindParam($this->fieldNumber++, $fieldValue, $columnMetadata['pdo_type']);
+            }
+
+            $statement->bindParam($this->fieldNumber, $this->rowNumber, \PDO::PARAM_INT);
+            $la_result = $statement->execute();
+
+            if (false === $la_result) {
+                throw new \Exception("Could not insert row $this->rowNumber into the database.");
+            }
+
+            $this->insertedIds[] = $statement->fetch(\PDO::FETCH_ASSOC);
+            $this->rowNumber++;
+        }
+    }
+
+
+    /**
+     * Do any data manipulation required on the specified column's value.
+     *
+     * @param array     $columnMetadata The metadata about the column.
+     * @param string    $fieldValue     The value to update.
+     *
+     * @return  string  The updated field value.
+     */
+    private function updateFieldValue($columnMetadata, $fieldValue)
+    {
+        if ('date' === $columnMetadata['type'] && 'default' !== $columnMetadata['format']) {
+            $fieldValue = self::isoDateFromFormat($columnMetadata['format'], $fieldValue);
+        }
+
+        if ('boolean' === $columnMetadata['type']) {
+            $fieldValue = self::booleanFromFilterBooleans($fieldValue);
+        }
+
+        if ('' === $fieldValue || '\N' === $fieldValue) {
+            $fieldValue = null;
+        }
+
+        return $fieldValue;
     }
 }
